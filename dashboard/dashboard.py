00bbeb82d3fa680ea8ac94387f98f33173d75080
Irwan <irwanseries1@gmail.com>
2024-10-01 21:16:41 +0700

    fixing png

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -50,12 +50,8 @@ with st.sidebar:
     st.title(" ")
 
     # Logo Image
-    #st.image("streamlit.png")
-    if os.path.exists("streamlit.png"):
-        st.image("streamlit.png")
-    else:
-        st.error("File not found: streamlit.png")
-
+    st.image("streamlit.png")
+   
     # Date Range
     start_date, end_date = st.date_input(
         label="Select Date Range",
